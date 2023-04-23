04caa48578e526c5631ec6722acfacbdb9301291
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-04-23 17:54:19 +0300

    hw04 SuperInputText. Class with disable

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,22 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+    const finalClassName = s.button +
+
+         (!disabled && xType === 'red' ? ' ' + s.red:  '') +
+         (disabled && xType === 'red' ? ' ' + s.disabled:  '') +
+
+         (!disabled && xType === 'secondary' ? ' ' + s.secondary:  '') +
+
+         (disabled  ? ' ' + s.disabled:  '') +
+        (!disabled && !xType ? ' ' + s.default:  '') +
+        (!disabled  ? ' ' + s.default:  '') +
+
         + (className ? ' ' + className : '') // задачка на смешивание классов
+
+    //         ? ...
+    //         : xType === 'red'
+    //             ? ...
 
     return (
         <button

951b1523dc3dc602f0a0afaa2a5481e0a65cb413
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-05-21 13:27:10 +0300

    hw08. added logic for sort by name (up, down), age filtration

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,31 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortByName = (a: UserType, b: UserType) => {
+                if (action.payload === 'up') {
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
+                }
+                if (action.payload === 'down') {
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) return 1
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) return -1
+                }
+                return 0
+            }
+            return [...state].sort(sortByName) // need to fix
+            // return [...state].sort((a,b) =>
+            // action.payload === 'up'
+            //     ? a.name.localeCompare(b.name)
+            //     : b.name.localeCompare(a.name)
+            // )
         }
+
         case 'check': {
 
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state

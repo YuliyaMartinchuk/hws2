0c988fac12cb5d734b821d746b944f07ce73d42a
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-06-06 22:57:32 +0300

    hw10. added logic for loadingReducer.ts, added logic for setLoading

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,14 +2,20 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type initStateType = typeof initState
+
+export const loadingReducer = (state = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state
     }
 }
+
 
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
